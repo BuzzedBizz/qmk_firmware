f7004ea932276a834206af2c61fb3bdbeee989ec
Chad Bissett <chadbissett@live.com>
2021-02-21 23:17:30 -0500

    Added the glow keyboard

---
+++ keyboards/lily58/glow/rules.mk
@@ -0,0 +1,6 @@
+SPLIT_KEYBOARD = yes
+SPLIT_TRANSPORT = mirror 
+
+RGB_MATRIX_DRIVER = WS2812
+
+LTO_ENABLE = yes 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
